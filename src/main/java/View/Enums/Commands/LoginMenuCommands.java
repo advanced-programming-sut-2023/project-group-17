5ab0b65177f63359a697c37aeb2a9aa61a9fb025
ShamimRahimi <119719139+ShamimRahimi@users.noreply.github.com>
2023-04-18 18:54:01 +0330

    updated login menu commands class

--- src/main/java/View/Enums/Commands/LoginMenuCommands.java
+++ src/main/java/View/Enums/Commands/LoginMenuCommands.java
@@ -1,4 +1,22 @@
 package View.Enums.Commands;
 
+import Controller.LoginMenuController;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum LoginMenuCommands {
+    USER_LOGIN(""),
+    FORGET_PASSWORD(""),
+    ENTER_SIGNUP_MENU("");
+
+    final String regex;
+    private LoginMenuCommands(String regex){
+        this.regex = regex;
+    }
+
+    public Matcher getMatcher(String input , LoginMenuCommands regex){
+        Matcher matcher = Pattern.compile(regex.regex).matcher(input);
+        return matcher.matches() ? matcher : null;
+    }
 }
