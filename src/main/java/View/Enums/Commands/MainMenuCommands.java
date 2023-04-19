6c37b82e7ceb15a96ddad4be629665537593aa0d
kasrahmi <a.kasrahmi@gmail.com>
2023-04-19 07:41:08 +0330

    main: Added Classes

--- src/main/java/View/Enums/Commands/MainMenuCommands.java
+++ src/main/java/View/Enums/Commands/MainMenuCommands.java
@@ -1,4 +1,18 @@
 package View.Enums.Commands;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum MainMenuCommands {
+    LOGOUT(""),
+    ENTER_PROFILE_MENU(""),
+    START_NEW_GAME("")
+    ;
+    String regex;
+    private MainMenuCommands(String regex) {this.regex = regex;}
+
+    public static Matcher getMatcher(String input, MainMenuCommands regex) {
+        Matcher matcher = Pattern.compile(regex.regex).matcher(input);
+        return matcher.matches() ? matcher : null;
+    }
 }
