51e97faca5864627e49c82ab40543f23a407eeda
kasrahmi <a.kasrahmi@gmail.com>
2023-04-19 07:50:38 +0330

    MainMenu: Structure Added

--- src/main/java/View/MainMenu.java
+++ src/main/java/View/MainMenu.java
@@ -1,4 +1,40 @@
 package View;
 
+import Controller.MainMenuController;
+import View.Enums.Commands.MainMenuCommands;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
 public class MainMenu {
+    private MainMenuController controller;
+
+    public MainMenu() {
+        this.controller = new MainMenuController();
+    }
+
+    public void run(Scanner scanner) {
+        Matcher matcher;
+        String command;
+
+        while (true) {
+            command = scanner.nextLine();
+            if ((matcher = MainMenuCommands.getMatcher(command, MainMenuCommands.ENTER_PROFILE_MENU)) != null)
+                enterProfileMenu();
+            else if ((matcher = MainMenuCommands.getMatcher(command, MainMenuCommands.LOGOUT)) != null)
+                logout();
+            else if ((matcher = MainMenuCommands.getMatcher(command, MainMenuCommands.START_NEW_GAME)) != null)
+                startNewGame(matcher);
+            else System.out.println("invalid command");
+        }
+    }
+
+
+    private void enterProfileMenu() {
+    }
+
+    private void logout() {
+    }
+    private void startNewGame(Matcher matcher) {
+    }
 }
