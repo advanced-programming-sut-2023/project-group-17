db932ce5852e21e7d1507aaac7d17ec8afc2b1c1
kasrahmi <a.kasrahmi@gmail.com>
2023-04-19 15:10:24 +0330

    BuildingMenu methods added

--- src/main/java/View/BuildingMenu.java
+++ src/main/java/View/BuildingMenu.java
@@ -1,11 +1,12 @@
 package View;
 
 import Controller.BuildingMenuController;
+import View.Enums.Commands.BuildingMenuCommands;
 
 import java.util.Scanner;
 import java.util.regex.Matcher;
 
-public class BuildingMenu {
+public class BuildingMenu extends Menu{
 
     BuildingMenuController controller;
     public BuildingMenu() {
@@ -16,7 +17,29 @@ public class BuildingMenu {
         String command;
         while (true) {
             command = scanner.nextLine();
+            if ((matcher = BuildingMenuCommands.getMatcher(command, BuildingMenuCommands.DROP_BUILDING)) != null)
+                dropBuilding(matcher);
+            else if ((matcher = BuildingMenuCommands.getMatcher(command, BuildingMenuCommands.SELECT_BUILDING)) != null)
+                selectBuilding(matcher);
+            else if ((matcher = BuildingMenuCommands.getMatcher(command, BuildingMenuCommands.DROP_BUILDING)) != null)
+                createUnit(matcher);
+            else if ((matcher = BuildingMenuCommands.getMatcher(command, BuildingMenuCommands.REPAIR)) != null)
+                repair();
+
+
         }
+    }
+
+    private void dropBuilding(Matcher matcher) {
+    }
+
+    private void selectBuilding(Matcher matcher) {
+    }
+
+    private void createUnit(Matcher matcher) {
+    }
+
+    private void repair() {
     }
 
 }
