3707a71f1e5012f913334203c313ab6c14844da7
kasrahmi <a.kasrahmi@gmail.com>
2023-04-19 14:15:10 +0330

    BuildingMenuCommands updated

--- src/main/java/View/BuildingMenu.java
+++ src/main/java/View/BuildingMenu.java
@@ -1,6 +1,22 @@
 package View;
 
+import Controller.BuildingMenuController;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
 public class BuildingMenu {
-    String push2;
+
+    BuildingMenuController controller;
+    public BuildingMenu() {
+        controller = new BuildingMenuController();
+    }
+    void run(Scanner scanner) {
+        Matcher matcher;
+        String command;
+        while (true) {
+            command = scanner.nextLine();
+        }
+    }
 
 }
