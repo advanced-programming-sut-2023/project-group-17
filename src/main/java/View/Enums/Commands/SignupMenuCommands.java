9c060074973607f633c53174782be950efbf6d54
AshkanTariverdi <ashkantariverdi82@gmail.com>
2023-04-20 15:19:22 +0430

    Signup Menu enums and regex completed *note* : regex only get number in the question-number field, anything else would be an Invalid Command

--- src/main/java/View/Enums/Commands/SignupMenuCommands.java
+++ src/main/java/View/Enums/Commands/SignupMenuCommands.java
@@ -1,4 +1,33 @@
 package View.Enums.Commands;
 
+import View.SignupMenu;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum SignupMenuCommands {
+    CREATE_USER("\\s*user\\s+create\\s+(?:(-u\\s+)(?<username>\".+\"|\\S+)?(\\s*)()|" +
+                    "(-p\\s+)(?<password>\".+\"|\\S+)?(\\s*)()|(-c\\s+)(?<confirmation>\".+\"|\\S+)?(\\s*)()|" +
+                    "(-email\\s+)(?<email>\".+\"|\\S+)?(\\s*)()|(-n\\s+)(?<nickname>\".+\"|\\S+)?(\\s*)()|" +
+                    "(-s\\s+)(?<slogan>\".+\"|\\S+)?(\\s*)()){5,}\\4\\8\\12\\16\\20"),
+    PICK_QUESTION("\\s*question\\s+pick\\s+(?:(-q\\s+)(?<questionNumber>\\d+)?(\\s*)()|" +
+                    "(-a\\s+)(?<answer>\".+\"|\\S+)?(\\s*)()|" +
+                    "(-c\\s+)(?<confirmation>\".+\"|\\S+)?(\\s*)()){3}\\4\\8\\12"),
+    ENTER_LOGIN_MENU("\\s*enter\\s+login\\s+menu\\s*")
+    ;
+
+    String regex;
+
+    private SignupMenuCommands(String regex) {
+        this.regex = regex;
+    }
+
+    public static Matcher getMatcher(String input, SignupMenuCommands regex) {
+        Matcher matcher = Pattern.compile(regex.regex).matcher(input);
+        if(matcher.matches())
+            return matcher;
+        return null;
+    }
+
+
 }
