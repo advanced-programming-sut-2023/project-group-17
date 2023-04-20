be8f03dee813f5aec4abad852907e1e4f3b581d3
ShamimRahimi <119719139+ShamimRahimi@users.noreply.github.com>
2023-04-20 12:28:08 +0330

    added methods of handling matcher group

--- src/main/java/View/Menu.java
+++ src/main/java/View/Menu.java
@@ -4,4 +4,13 @@ import java.util.Scanner;
 
 public abstract class Menu {
     abstract void run(Scanner scanner);
+
+    public static String handleDoubleQuote(String matcherGroup) {
+        return (matcherGroup.startsWith("\"") && matcherGroup.endsWith("\"")) ?
+                matcherGroup.substring(1 , matcherGroup.length() - 1) : matcherGroup;
+    }
+
+    public static boolean checkBlankField(String matcherGroup) {
+        return matcherGroup==null;
+    }
 }
