53d73d9b9b4b095a5db14bca31d4f845d82f5905
AshkanTariverdi <ashkantariverdi82@gmail.com>
2023-04-22 01:11:31 +0430

    shop menu methods added

--- src/main/java/Controller/ShopMenuController.java
+++ src/main/java/Controller/ShopMenuController.java
@@ -1,4 +1,17 @@
 package Controller;
 
+import View.Enums.Messages.ShopMenuMessages;
+
 public class ShopMenuController {
+    public String showPriceList() {
+        return null;
+    }
+
+    public ShopMenuMessages buyItem(String itemName, Integer amount) {
+        return null;
+    }
+
+    public ShopMenuMessages sellItem(String itemName, Integer amount) {
+        return null;
+    }
 }
--- src/main/java/View/ShopMenu.java
+++ src/main/java/View/ShopMenu.java
@@ -35,14 +35,59 @@ public class ShopMenu extends Menu{
     }
 
     private void showPriceList() {
-
+        System.out.println(controller.showPriceList());
     }
 
     private void buyItem(Matcher matcher) {
+        if(checkBlankField(matcher.group("name")) || checkBlankField(matcher.group("amount")))
+            System.out.println("buy item failed : blank field");
+
+        String itemName = handleDoubleQuote(matcher.group("name"));
+        Integer amount = Integer.parseInt(matcher.group("amount"));
+
+        switch (controller.buyItem(itemName, amount)) {
+            case SUCCESS:
+                System.out.println("item bought successfully");
+                break;
+            case NOT_ENOUGH_COIN:
+                System.out.println("buy item failed : not enough coin");
+                break;
+            case ITEM_DOES_NOT_EXISTS:
+                System.out.println("buy item failed : this item is not in the shop currently");
+                break;
+            case NOT_ENOUGH_SPACE:
+                System.out.println("buy item failed : there is not enough space to buy this item");
+                break;
+            case INVALID_ITEM_NAME:
+                System.out.println("buy item failed : invalid item name");
+                break;
+            case INVALID_ITEM_AMOUNT:
+                System.out.println("buy item failed : invalid item amount");
+                break;
+        }
 
     }
 
     private void sellItem(Matcher matcher) {
+        if(checkBlankField(matcher.group("name")) || checkBlankField(matcher.group("amount")))
+            System.out.println("buy item failed : blank field");
 
+        String itemName = handleDoubleQuote(matcher.group("name"));
+        Integer amount = Integer.parseInt(matcher.group("amount"));
+
+        switch (controller.sellItem(itemName, amount)) {
+            case SUCCESS:
+                System.out.println("item sold successfully");
+                break;
+            case INVALID_ITEM_AMOUNT:
+                System.out.println("sell item failed : invalid item amount");
+                break;
+            case INVALID_ITEM_NAME:
+                System.out.println("sell item failed : invalid item name");
+                break;
+            case ITEM_DOES_NOT_EXISTS:
+                System.out.println("sell item failed : you don't have this item");
+                break;
+        }
     }
 }
