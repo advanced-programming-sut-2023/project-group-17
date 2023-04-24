975456c3e1be8f0c30d7252b35baeae5dc3e2eb4
ShamimRahimi <119719139+ShamimRahimi@users.noreply.github.com>
2023-04-24 14:34:17 +0330

    attack tools and methods added

--- src/main/java/Model/AttackToolsAndMethods/AttackToolsAndMethods.java
+++ src/main/java/Model/AttackToolsAndMethods/AttackToolsAndMethods.java
@@ -1,4 +1,72 @@
 package Model.AttackToolsAndMethods;
 
+import Model.User;
+
 public class AttackToolsAndMethods {
+    private final User owner;
+    private final String name;
+    private final int numberOfEngineers;
+    private final boolean isPortable;
+    private int hp;
+    private final int range;
+    private final int damage;
+    private final int speed;
+    private final int accuracy;
+    private final int power;
+    public AttackToolsAndMethods(User owner, AttackToolsAndMethodsType type) {
+        this.owner = owner;
+        this.name = type.getName();
+        this.numberOfEngineers = type.getNumberOfEngineers();
+        this.isPortable = type.isPortable();
+        this.hp = type.getHp();
+        this.range = type.getRange();
+        this.damage = type.getDamage();
+        this.accuracy = type.getAccuracy();
+        this.speed = type.getSpeed();
+        this.power = type.getPower();
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfEngineers() {
+        return numberOfEngineers;
+    }
+
+    public boolean isPortable() {
+        return isPortable;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getAccuracy() {
+        return accuracy;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void changeHp(int hp) {
+        this.hp += hp;
+    }
 }
