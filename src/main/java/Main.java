9d7f9d56cf46a8f714854ddd4ec63cc7d855293f
AshkanTariverdi <ashkantariverdi82@gmail.com>
2023-04-25 20:42:19 +0430

    delete comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,9 @@
 import View.LoginMenu;
 
-import java.util.Scanner;
-
 import static View.Menu.scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
         new LoginMenu().run(scanner);
     }
 }
