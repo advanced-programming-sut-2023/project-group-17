0e0b3ebdb88337ba46e3286398a19263670cca13
kasrahmi <a.kasrahmi@gmail.com>
2023-05-02 16:09:59 +0330

    storage for each production building convert to string

--- src/main/java/Model/Buildings/ProductionBuilding.java
+++ src/main/java/Model/Buildings/ProductionBuilding.java
@@ -8,11 +8,13 @@ import java.util.HashMap;
 
 public class ProductionBuilding extends Building{
     private HashMap<Item.ItemType, Item.ItemType> productionItem;
-    private Building relatedStorageBuilding;
+    private String relatedStorageBuildingName;
     int rate;
-    public ProductionBuilding(User owner, int number, Building building, int x, int y) {
+    public ProductionBuilding(User owner, int number, Building building, int x, int y, ProductionBuildingType.ProductionType productionType) {
         //TODO: constructor hameye buildinga
         super(owner, building, x, y);
+        this.productionItem = productionType.getProductionItem();
+        this.relatedStorageBuildingName = productionType.getRelatedStorageBuildingName();
     }
 
     public HashMap<Item.ItemType, Item.ItemType> getProductionItem() {
@@ -21,5 +23,9 @@ public class ProductionBuilding extends Building{
 
     public int getRate() {
         return rate;
+    }
+
+    public String getRelatedStorageBuildingName() {
+        return relatedStorageBuildingName;
     }
 }
--- src/main/java/Model/Buildings/ProductionBuildingType.java
+++ src/main/java/Model/Buildings/ProductionBuildingType.java
@@ -8,22 +8,22 @@ import java.util.HashMap;
 
 public class ProductionBuildingType {
     public enum ProductionType {
-        MILL(new HashMap<>(){{put(Item.ItemType.HOPS, Item.ItemType.WHEAT);}}, "mill", Database.getBuildingDataByName("stockpile")),
-        ARMOURER(new HashMap<>(){{put(Item.ItemType.METAL_ARMOR, Item.ItemType.IRON);}}, "armourer", Database.getBuildingDataByName("armoury")),
-        BLACKSMITH(new HashMap<>(){{put(Item.ItemType.SWORDS, Item.ItemType.IRON);put(Item.ItemType.MACE, Item.ItemType.IRON);}}, "blacksmith", Database.getBuildingDataByName("armoury")),
-        FLETCHER(new HashMap<>(){{put(Item.ItemType.BOW, Item.ItemType.WOOD);}}, "fletcher", Database.getBuildingDataByName("armoury")),
-        POLETURNER(new HashMap<>(){{put(Item.ItemType.SPEAR, Item.ItemType.WOOD);put(Item.ItemType.PIKE, Item.ItemType.WOOD);}}, "poleturner", Database.getBuildingDataByName("armoury")),
-        DAIRY(new HashMap<>(){{put(Item.ItemType.CHEESE, Item.ItemType.COW);put(Item.ItemType.LEATHER_ARMOR, Item.ItemType.COW);}}, "dairy", Database.getBuildingDataByName("armoury")),
-        BAKERY(new HashMap<>(){{put(Item.ItemType.BREAD, Item.ItemType.FLOUR);}}, "bakery", Database.getBuildingDataByName("granary")),
-        BREWER(new HashMap<>(){{put(Item.ItemType.ALE, Item.ItemType.HOPS);}}, "brewer", Database.getBuildingDataByName("inn"))
+        MILL(new HashMap<>(){{put(Item.ItemType.HOPS, Item.ItemType.WHEAT);}}, "mill", "stockpile"),
+        ARMOURER(new HashMap<>(){{put(Item.ItemType.METAL_ARMOR, Item.ItemType.IRON);}}, "armourer", "armoury"),
+        BLACKSMITH(new HashMap<>(){{put(Item.ItemType.SWORDS, Item.ItemType.IRON);put(Item.ItemType.MACE, Item.ItemType.IRON);}}, "blacksmith", "armoury"),
+        FLETCHER(new HashMap<>(){{put(Item.ItemType.BOW, Item.ItemType.WOOD);}}, "fletcher", "armoury"),
+        POLETURNER(new HashMap<>(){{put(Item.ItemType.SPEAR, Item.ItemType.WOOD);put(Item.ItemType.PIKE, Item.ItemType.WOOD);}}, "poleturner", "armoury"),
+        DAIRY(new HashMap<>(){{put(Item.ItemType.CHEESE, Item.ItemType.COW);put(Item.ItemType.LEATHER_ARMOR, Item.ItemType.COW);}}, "dairy", "armoury"),
+        BAKERY(new HashMap<>(){{put(Item.ItemType.BREAD, Item.ItemType.FLOUR);}}, "bakery", "granary"),
+        BREWER(new HashMap<>(){{put(Item.ItemType.ALE, Item.ItemType.HOPS);}}, "brewer", "inn")
         ;
         private HashMap<Item.ItemType, Item.ItemType> productionItem;
         private String name;
-        private Building relatedStorageBuilding;
-        ProductionType(HashMap<Item.ItemType, Item.ItemType> productionItem, String name, Building relatedStorageBuilding) {
+        private String relatedStorageBuildingName;
+        ProductionType(HashMap<Item.ItemType, Item.ItemType> productionItem, String name, String relatedStorageBuildingName) {
             this.productionItem = productionItem;
             this.name = name;
-            this.relatedStorageBuilding = relatedStorageBuilding;
+            this.relatedStorageBuildingName = relatedStorageBuildingName;
         }
 
         public HashMap<Item.ItemType, Item.ItemType> getProductionItem() {
@@ -34,8 +34,8 @@ public class ProductionBuildingType {
             return name;
         }
 
-        public Building getRelatedStorageBuilding() {
-            return relatedStorageBuilding;
+        public String getRelatedStorageBuildingName() {
+            return relatedStorageBuildingName;
         }
     }
     public static ProductionType getProductionType(String name) {
