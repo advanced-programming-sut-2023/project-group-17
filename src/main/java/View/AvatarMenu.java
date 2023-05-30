4960ed18ba3c961128ec46d8721690287a66aaec
kasrahmi <a.kasrahmi@gmail.com>
2023-05-30 23:16:27 +0330

    drag and drop method added

--- src/main/java/View/AvatarMenu.java
+++ src/main/java/View/AvatarMenu.java
@@ -13,10 +13,16 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -38,14 +44,90 @@ public class AvatarMenu extends Application {
                 "/assets/Backgrounds/avatarMenu.jpg").toExternalForm()),
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,
                 new BackgroundSize(1.0, 1.0, true, true, false, false))));
-//        Dragboard dragboard = new
+
+        Label label = new Label("Drop your avatar here");
+        makeLabelDropAvatar(label);
+
         makeHBoxBrowse(pane, stage);
+
         makeVBoxDefaultAvatars(pane, stage);
+
+        pane.getChildren().add(label);
         AvatarMenu.pane = pane;
         stage.getScene().setRoot(pane);
         stage.setFullScreen(true);
         stage.show();
     }
+
+    private void makeLabelDropAvatar(Label label) {
+        label.setLayoutX(650); label.setLayoutY(100); label.setAlignment(Pos.CENTER);
+        label.setTextFill(Color.BLACK); label.setMinWidth(160); label.setMinHeight(160); label.setStyle("-fx-background-color: #daa520;");
+        label.setOnDragOver(event -> {
+            if (event.getGestureSource() != label && event.getDragboard().hasFiles()) {
+                event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+            }
+            event.consume();
+        });
+        label.setOnDragDropped(event -> {
+            Dragboard db = event.getDragboard();
+            boolean success = false;
+            if (db.hasFiles()) {
+                File file = db.getFiles().get(0);
+                Image image = new Image(file.toURI().toString());
+                controller.changeAvatarCustom(file.toURI().toString());
+//                avatar.setImage(image);
+                success = true;
+            }
+            event.setDropCompleted(success);
+            event.consume();
+        });
+    }
+
+//    private void handleTargetNode(ImageView sourceNode, Pane targetNode) {
+//        sourceNode.setOnDragDetected(event -> {
+//            Dragboard db = sourceNode.startDragAndDrop(TransferMode.ANY);
+//            ClipboardContent content = new ClipboardContent();
+//            content.putImage(sourceNode.getImage());
+//            db.setContent(content);
+//            event.consume();
+//        });
+//        targetNode.setOnDragOver(event -> {
+//            if (event.getGestureSource() != targetNode && event.getDragboard().hasImage()) {
+//                event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+//            }
+//            event.consume();
+//        });
+//
+//        targetNode.setOnDragEntered(event -> {
+//            if (event.getGestureSource() != targetNode && event.getDragboard().hasImage()) {
+//                targetNode.setStyle("-fx-border-color: blue;");
+//            }
+//            event.consume();
+//        });
+//
+//        targetNode.setOnDragExited(event -> {
+//            targetNode.setStyle("-fx-border-color: black;");
+//            event.consume();
+//        });
+//        targetNode.setOnDragDropped(event -> {
+//            Dragboard db = event.getDragboard();
+//            boolean success = false;
+//            if (db.hasImage()) {
+//                ImageView imageView = new ImageView(db.getImage());
+//                targetNode.getChildren().add(imageView);
+//                success = true;
+//            }
+//            event.setDropCompleted(success);
+//            event.consume();
+//        });
+//        sourceNode.setOnDragDone(event -> {
+//            if (event.getTransferMode() == TransferMode.MOVE) {
+//                // Remove the source node if it was moved
+//                sourceNode.setImage(null);
+//            }
+//            event.consume();
+//        });
+//    }
 
     @FXML
     public void initialize() {
